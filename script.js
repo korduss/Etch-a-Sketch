eb41a17e72163fbc2c22357b9a61d03c882f5526
korduss <bernatkordian23@gmail.com>
2023-08-17 10:07:00 +0200

    create 16x16 grid of squares

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("tewst");
+const container = document.querySelector('#container');
+// const singleSquare = document.createElement('div');
+//singleSquare.className = 'square';
+//singleSquare.setAttribute('style','background-color: grey;');
+//singleSquare.textContent = 'i';
+
+for(i = 0; i < 256; i++){
+    const singleSquare = document.createElement('div');
+    singleSquare.className = 'square';
+    singleSquare.setAttribute('style','background-color: grey;');
+    
+    container.appendChild(singleSquare)
+
+}

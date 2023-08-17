9b2f51ad1e2b9adb8ebc6f646c4adf09af39b554
korduss <bernatkordian23@gmail.com>
2023-08-17 10:19:30 +0200

    add 'draw-system'

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const container = document.querySelector('#container');
-// const singleSquare = document.createElement('div');
-//singleSquare.className = 'square';
-//singleSquare.setAttribute('style','background-color: grey;');
-//singleSquare.textContent = 'i';
 
 for(i = 0; i < 256; i++){
     const singleSquare = document.createElement('div');
     singleSquare.className = 'square';
-    singleSquare.setAttribute('style','background-color: grey;');
-    
     container.appendChild(singleSquare)
+};
+const squares = document.querySelectorAll('.square');
 
-}
+squares.forEach((square)=>{
+    square.addEventListener('mouseenter', ()=>{
+        square.classList.add('blackSquare');
+    });
+})

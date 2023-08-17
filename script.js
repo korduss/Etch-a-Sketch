f727b66d319ebadeba9414c2f674301a4af6dd2b
korduss <bernatkordian23@gmail.com>
2023-08-17 11:00:23 +0200

    create better grid

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
-const container = document.querySelector('#container');
+const mainContainer = document.querySelector('#mainContainer');
 
-for(i = 0; i < 256; i++){
-    const singleSquare = document.createElement('div');
-    singleSquare.className = 'square';
-    container.appendChild(singleSquare)
-};
+
+//Create grid of squares
+for(i = 0; i < 16; i++){
+    const container = document.createElement('div');
+    container.className = 'container';
+    mainContainer.appendChild(container);
+    for(x = 0; x < 16; x++){
+        const singleSquare = document.createElement('div');
+        singleSquare.className = 'square';
+        container.appendChild(singleSquare)
+    }
+}
+//Draw system
 const squares = document.querySelectorAll('.square');
-
 squares.forEach((square)=>{
     square.addEventListener('mouseenter', ()=>{
         square.classList.add('blackSquare');
-    });
+    })
 })
